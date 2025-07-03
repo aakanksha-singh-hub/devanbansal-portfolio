d7e9564e3b28874ac595253f55f024bdc957aa40
Aditi Singh <aditi.singh0205@gmail.com>
2025-07-03 23:43:55 +0530

    refactor: improve HTML structure and update styles in Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,29 +1,29 @@
-
-import React from 'react';
-import Navigation from '@/components/Navigation';
-import HeroSection from '@/components/HeroSection';
-import AboutSection from '@/components/AboutSection';
-import EducationSection from '@/components/EducationSection';
-import SectionDivider from '@/components/SectionDivider';
-import ProjectsSection from '@/components/ProjectsSection';
-import AchievementsSection from '@/components/AchievementsSection';
-import ExperienceSection from '@/components/ExperienceSection';
-import SkillsSection from '@/components/SkillsSection';
-import ContactSection from '@/components/ContactSection';
+import React from "react";
+import Navigation from "@/components/Navigation";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import EducationSection from "@/components/EducationSection";
+import SectionDivider from "@/components/SectionDivider";
+import ProjectsSection from "@/components/ProjectsSection";
+import AchievementsSection from "@/components/AchievementsSection";
+import ExperienceSection from "@/components/ExperienceSection";
+import SkillsSection from "@/components/SkillsSection";
+import ContactSection from "@/components/ContactSection";
 
 const Index = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-black">
       <Navigation />
-      <HeroSection />
-      <AboutSection />
-      <EducationSection />
-      <SectionDivider />
-      <ProjectsSection />
-      <AchievementsSection />
-      <ExperienceSection />
-      <SkillsSection />
-      <ContactSection />
+      <div className="px-4 sm:px-6 lg:px-8 max-w-screen-2xl mx-auto">
+        <HeroSection />
+        <AboutSection />
+        <EducationSection />
+        <ProjectsSection />
+        <AchievementsSection />
+        <ExperienceSection />
+        <SkillsSection />
+        <ContactSection />
+      </div>
     </div>
   );
 };
